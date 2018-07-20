666bbd2fb22dc900d75f55a083d2d9a22bfa51b5
Junior Abranches <jr-abranches@hotmail.com>
2018-07-19 23:04:42 -0400

    Secao 5 - Aula 3

--- lib/cards_page.dart
+++ lib/cards_page.dart
@@ -38,6 +38,68 @@ final List<CardViagem> destinos = <CardViagem>[
 ];
 
 
+class CardItens extends StatelessWidget {
+  CardItens({Key key, @required this.destinos, this.shape}) : super(key: key);
+
+  static const double height = 160.0;
+  final CardViagem destinos;
+  final ShapeBorder shape;
+
+  @override
+  Widget build(BuildContext context) {
+    final ThemeData theme = Theme.of(context);
+    final TextStyle descricaoStyle = theme.textTheme.subhead;
+
+    return SafeArea (
+      top: false,
+      bottom: false,
+      child: Container (
+        padding: const EdgeInsets.all(8.0),
+        height: height,
+        child: Card (
+          shape: shape,
+          child: Column (
+            crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                  child: Text(
+                    destinos.titulo,
+                    style: descricaoStyle.copyWith(color: Colors.redAccent)
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+                    child: DefaultTextStyle(
+                      softWrap: false,
+                      overflow: TextOverflow.ellipsis,
+                      style: descricaoStyle,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(bottom: 8.0),
+                            child: Text(
+                              destinos.descricao[0],
+                              style: descricaoStyle.copyWith(color: Colors.green)
+                            ),
+                          ),
+                          Text(destinos.descricao[1]),
+                          Text(destinos.descricao[2])
+                        ],
+                      )
+                    )
+                  )
+                )
+              ],
+          )
+         ),
+       ),
+     );
+   }
+}
+
 class CardsPage extends StatefulWidget {
   @override
   _CardsPageState createState() => new _CardsPageState();
@@ -73,7 +135,14 @@ class _CardsPageState extends State<CardsPage>{
           ],
         ),
         body: new ListView(
-
+          itemExtent: CardItens.height,
+          padding: EdgeInsets.only(top: 8.0, left: 8.0, right: 8.0, bottom: 0.0),
+          children: destinos.map((CardViagem dest) {
+            return Container(
+              margin: const EdgeInsets.only(bottom: 8.0),
+              child: CardItens(destinos: dest, shape: _shape),
+            );
+          }).toList()
         )
     );
   }
